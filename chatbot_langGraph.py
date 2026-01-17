e60bdcb261da1485d787fe1e9c7a55087e670ba5
meharaz733 <meharaz733@gmail.com>
2026-01-17 15:15:05 +0600

    chatbot_implement

--- chatbot_langGraph.py
+++ chatbot_langGraph.py
@@ -1,1 +1,36 @@
 from langgraph.graph import StateGraph, START, END, add_messages
+from typing import TypedDict, Annotated, List
+from langchain_core.messages import BaseMessage, HumanMessage, AIMessage
+from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
+
+class ChatState(TypedDict):
+    messages: Annotated[List[BaseMessage], add_messages]
+
+llm_model = HuggingFaceEndpoint(
+    repo_id = "meta-llama/Llama-3.1-8B-Instruct",
+    task = "text_generation"
+)
+
+model = ChatHuggingFace(llm=llm_model)
+
+
+def chat(state: ChatState)->ChatState:
+    response = model.invoke(state['messages'])
+
+    return {
+        'messages': [response]
+    }
+
+main_graph = StateGraph(ChatState)
+
+main_graph.add_node('chat', chat)
+
+main_graph.add_edge(START, 'chat')
+main_graph.add_edge('chat', END)
+
+workflow = main_graph.compile()
+
+ans = workflow.invoke({
+                    'messages':[HumanMessage(content="What is the name of Bangladeshi primeminister?")]
+                })
+print(ans)
