4d9cac7e9deeead1bccb8b10f8e01d0701ce1600
meharaz733 <meharaz733@gmail.com>
2026-01-17 20:36:43 +0600

    memory implemented

--- chatbot_langGraph.py
+++ chatbot_langGraph.py
@@ -2,6 +2,7 @@ from langgraph.graph import StateGraph, START, END, add_messages
 from typing import TypedDict, Annotated, List
 from langchain_core.messages import BaseMessage, HumanMessage, AIMessage
 from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
+from langgraph.checkpoint.memory import MemorySaver
 
 class ChatState(TypedDict):
     messages: Annotated[List[BaseMessage], add_messages]
@@ -23,14 +24,30 @@ def chat(state: ChatState)->ChatState:
 
 main_graph = StateGraph(ChatState)
 
+checkpoint = MemorySaver()
+
 main_graph.add_node('chat', chat)
 
 main_graph.add_edge(START, 'chat')
 main_graph.add_edge('chat', END)
 
-workflow = main_graph.compile()
+workflow = main_graph.compile(checkpointer=checkpoint)
 
-ans = workflow.invoke({
-                    'messages':[HumanMessage(content="What is the name of Bangladeshi primeminister?")]
-                })
-print(ans)
+# ans = workflow.invoke({
+                    # 'messages':[HumanMessage(content="What is the name of Bangladeshi primeminister?")]
+                # })
+# print(ans)
+thread_id = '1'
+
+while True:
+    user_msg = input("Type here:")
+    print('User:', user_msg)
+    if user_msg.strip().lower() in ['bye', 'exit', 'quit']:
+        break
+    config = {'configurable':{'thread_id':thread_id}}
+    
+    response = workflow.invoke({
+        'messages':[HumanMessage(content=user_msg)]
+    }, config=config)
+
+    print('AI:',response['messages'][-1].content)
