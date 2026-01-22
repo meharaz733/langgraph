53a69bb818a31588581974250b2eed74e5ccabef
meharaz733 <meharaz733@gmail.com>
2026-01-23 01:25:31 +0600

    [add]Chat Stream Feature

--- chatbot_frontend_streamlit.py
+++ chatbot_frontend_streamlit.py
@@ -11,6 +11,36 @@ for message in st.session_state['message_history']:
 
 user_input = st.chat_input('type here...')
 
+#normal way to print llm response
+# if user_input:
+#     st.session_state['message_history'].append(
+#         {
+#             'role':'user',
+#             'content':user_input
+#         }
+#     )
+#     with st.chat_message('user'):
+#         st.text(user_input)
+
+#     response = workflow.invoke(
+#         {
+#             'messages':[HumanMessage(content=user_input)]
+#         },
+#         config=CONFIG
+#     )
+
+#     st.session_state['message_history'].append(
+#         {
+#             'role':'assistant',
+#             'content':response['messages'][-1].content
+#         }
+#     )
+
+#     with st.chat_message('assistant'):
+#         st.text(response['messages'][-1].content)
+
+
+#streaming llm response
 if user_input:
     st.session_state['message_history'].append(
         {
@@ -21,19 +51,22 @@ if user_input:
     with st.chat_message('user'):
         st.text(user_input)
 
-    response = workflow.invoke(
+    response = workflow.stream(
         {
             'messages':[HumanMessage(content=user_input)]
         },
-        config=CONFIG
+        config=CONFIG,
+        stream_mode='messages'
     )
-
+    
+    with st.chat_message('assistant'):
+        ai_msg = st.write_stream(
+                        msg.content for msg, metadata in response
+                    )
+    
     st.session_state['message_history'].append(
         {
             'role':'assistant',
-            'content':response['messages'][-1].content
+            'content': ai_msg
         }
     )
-
-    with st.chat_message('assistant'):
-        st.text(response['messages'][-1].content)
