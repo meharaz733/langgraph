c92c3d7a904270161f383188cbfd46de9827460d
meharaz733 <meharaz733@gmail.com>
2026-01-23 17:56:33 +0600

    [Feature] sidebar, dynamic thread id

--- chatbot_frontend_streamlit.py
+++ chatbot_frontend_streamlit.py
@@ -1,9 +1,36 @@
 import streamlit as st
-from chatbot_langGraph import CONFIG, workflow
+from chatbot_langGraph import workflow
 from langchain.messages import HumanMessage
+import uuid
 
+
+#utility func
+def gen_thread_id():
+    return uuid.uuid4()
+
+
+#config
 if 'message_history' not in st.session_state:
     st.session_state['message_history'] = []
+if 'thread_id' not in st.session_state:
+    st.session_state['thread_id'] = gen_thread_id()
+
+CONFIG = {'configurable':{'thread_id':st.session_state['thread_id']}}
+
+
+
+#Sidebar
+st.sidebar.title("ChatBot")
+
+st.sidebar.button('New Chat')
+
+st.sidebar.header("My Conversation")
+
+st.sidebar.text(st.session_state['thread_id'])
+
+
+#main part
+
 
 for message in st.session_state['message_history']:
     with st.chat_message(message['role']):
@@ -12,6 +39,8 @@ for message in st.session_state['message_history']:
 user_input = st.chat_input('type here...')
 
 #normal way to print llm response
+#
+# 
 # if user_input:
 #     st.session_state['message_history'].append(
 #         {
@@ -40,7 +69,9 @@ user_input = st.chat_input('type here...')
 #         st.text(response['messages'][-1].content)
 
 
+
 #streaming llm response
+# 
 if user_input:
     st.session_state['message_history'].append(
         {
